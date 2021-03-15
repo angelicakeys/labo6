7ce8d535ac19c30c7de54db8fdb242900ef71eef
angelicakeys <75345767+angelicakeys@users.noreply.github.com>
2021-03-15 15:41:25 -0400

    sections des pages ajoute

--- themes/angelica/front-page.php
+++ themes/angelica/front-page.php
@@ -21,38 +21,41 @@ get_header();
 				?>
 			</header><!-- .page-header -->
 
+			<section class="cours">
 			<?php
 			/* Start the Loop */
             $precedent = 0;
 			while ( have_posts() ) :
 				the_post();
                 $titre = get_the_title();
+				//582-1W1 Mise en page Web(75h)
+				$sigle = substr($titre, 0,7);
+				$nbHeure = substr($title,-4,3);
+				$titrePartiel = substr($titre,8,-6);
+
                 $session = substr($titre, 4,1);
-                $contenu = (get_the_content());
-                $resume = substr($contenu, 0, 200);
+                //$contenu = (get_the_content());
+               // $resume = substr($contenu, 0, 200);
 				$typeCours = get_field('type_de_cours');
                 
             ?>
 
-            <?php
-                if($session != $precedent){
-                    echo "<p>Session : " . $session . "</p>";
-                }
-                $precedent = $session;
-            ?>
-                
-            <p> <?php  echo $typeCours . " - " . $session . " - " . $titre; ?> <p>
-			<p> <?php echo $resume; ?> </p>
-            <?php
-			endwhile;
+				<article>
+				<p><?php echo $sigle . " . "  . $typeCours; ?> </p>
+				<a href="<?php echo get_permalink()?>"><?php echo $titrePartiel;?></a>
+				<p> Section :<?php echo $session ?> </p>
 
-			
+				</article>
+
+          
+            <?php
+			endwhile;?>
 
 		
 
 		
-
-		endif;
+	</section> <!-- fin de section cours -->
+		<?php endif;?>
 		?>
 
 	</main><!-- #main -->
--- themes/angelica/functions.php
+++ themes/angelica/functions.php
@@ -203,15 +203,20 @@ function extraire_cours($query){
 	}
 }
 add_action('pre_get_posts', 'extraire_cours');
-
+/*
+L'adaptation de la requete par defaut on accede a la page d'accueil
+*/
 function extraire_cours_front_page($query){
 	if( !is_admin() && $query->is_front_page() && $query->is_main_query() ){
 
 	$query->set( 'category_name', 'cours' );
 	$query->set('posts_per_page', -1 );
-	$query->set('orderby', 'title');
-	$query->set('order', 'desc');
-	
+	$query->set('meta_key', 'type_de_cours');
+	//$query->set('orderby', 'meta_value');
+
+	$query->set('orderby', array ('meta_value' => 'DESC', 'title' => 'ASC'));
+	//$query->set('order', 'desc');
+
 	}
 }
 
