e3f256ff815c3cb1420a37fe1388b706f19ad94d
angelicakeys <75345767+angelicakeys@users.noreply.github.com>
2021-03-18 22:30:51 -0400

    texte center titre section et slider responsive

--- themes/angelica/front-page.php
+++ themes/angelica/front-page.php
@@ -18,7 +18,7 @@ get_header();
 			<header class="page-header">
 			<h1 class="page-title">Cours </h1> 
 				<?php
-				/* the_archive_title( '<h1 class="page-title">', '</h1>' ); */
+				
 
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
